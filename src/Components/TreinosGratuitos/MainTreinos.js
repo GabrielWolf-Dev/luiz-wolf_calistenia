d565008f5f577a014fa5279c833622f3ddf90e34
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2021-04-01 11:01:44 -0300

    Update
    Adding a gif for representation of website in the README.

--- src/Components/TreinosGratuitos/MainTreinos.js
+++ src/Components/TreinosGratuitos/MainTreinos.js
@@ -76,6 +76,35 @@ export default function MainTreinos({ theme }) {
             avançado, com o uso mínimo de equipamentos e alguns com uso de equipamentos também,
             conseguindo trabalhar qualquer parte do seu corpo de maneira eficiente.
           </DefaultParagraph>
+          <DefaultParagraph
+            as={motion.p}
+            variants={{
+              show: { opacity: 1, x: 0 },
+              hidden: { opacity: 0, x: 60 },
+            }}
+            transition={{
+              duration: 0.6,
+              delay: 0.8,
+            }}
+            initial="hidden"
+            animate="show"
+          >
+            Se você quiser mais treinos e dicas é só me seguir no instagram
+            {' '}
+            <a
+              style={{
+                fontSize: '1.4rem',
+                fontWeight: '400',
+                padding: '16px 2%',
+                color: '#333333',
+              }}
+              href="https://www.instagram.com/luizmiguelwolf/"
+            >
+              @luizmiguelwolf
+            </a>
+            ,
+            lá você também terá mais interação comigo.
+          </DefaultParagraph>
         </SectionContent>
       </ContainerFlex>
     </BoxMTreinos>
